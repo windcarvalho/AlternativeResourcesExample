813e34adcd2f4e29c5719d744ed4265854f6c206
Windson Viana <windson@virtual.ufc.br>
2023-05-12 09:12:14 -0300

    Commit Inicial Projeto com Recursos Alternativos

---
+++ app/src/main/java/com/example/alternativeresourcesexample/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.alternativeresourcesexample
+
+import android.content.res.Configuration
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.OnConfigurationChangedProvider
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun increaseFont(v:View){
+        var t:TextView=findViewById(R.id.textView)
+        var densidadeDatela:Float= getResources().getDisplayMetrics().scaledDensity //Pegando as dimensões da tela
+        var novotamanho:Float= (t.textSize/densidadeDatela +2) //Aumentando em 2sp a fonte
+        t.setTextSize(novotamanho) //mudando o tamanho
+    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        //Esse método só é chamado se houver declaração no Android Manifest
+
+        var bt= findViewById<Button>(R.id.button)
+        //Cconfere se mudou para Landscape
+        if (newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+            bt.setBackgroundColor(Color.CYAN)
+        }else{
+            bt.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
